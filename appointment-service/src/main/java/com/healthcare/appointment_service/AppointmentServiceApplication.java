ccde5997d24011f880b582fe902228086f9014b5
Asif2466 <asif.mohideen413@gmail.com>
2024-08-28 21:43:30 +0530

    init

--- appointment-service/src/main/java/com/healthcare/appointment_service/AppointmentServiceApplication.java
+++ appointment-service/src/main/java/com/healthcare/appointment_service/AppointmentServiceApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableDiscoveryClient
 @SpringBootApplication
-@EnableFeignClients // Enable Feign client
+@EnableFeignClients
+@EnableDiscoveryClient// Enable Feign client
 public class AppointmentServiceApplication {
 
 	public static void main(String[] args) {
